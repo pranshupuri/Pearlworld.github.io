12ee12b3fded8fe5fa2f7401c07b0fdf5c5522f6
Manoj Nama <manoj.nama@outlook.com>
2015-01-24 22:51:58 +0530

    Added nodemailer feature

--- app.js
+++ app.js
@@ -1,13 +1,41 @@
 var express = require('express');
 var newrelic = require('newrelic');
 var path = require('path');
+var bodyParser = require('body-parser');
+var nodemailer = require('nodemailer');
 
 var app = express();
+app.use(bodyParser());
+// var transporter = nodemailer.createTransport();
+var transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+        user: 'nama0supercool@gmail.com',
+        pass: 'wapakoneta2'
+    }
+});
 
 app.use(express.static(__dirname + "/public"));
 
 app.get('/available', function (req, res) {
 	res.end("YES");
+});
+
+app.post("/contact", function (req, res) {
+	var body = req.body;
+	
+	transporter.sendMail({
+	    from: [{name: body.firstName, address: body.email}],
+	    to: 'hello@vishalnama.com',
+	    subject: body.subject,
+	    html: '<h2>' + body.firstName + ' ' + body.lastName + '</h2><br><p>' + body.email + '</p><p>' + body.message + '</p>' 
+	}, function (err, resp) {
+		console.log(err, resp);
+		if(resp) {
+			console.log("Mail Sent!!!");
+		}
+		res.redirect("/");
+	});
 });
 
 app.listen(process.env.PORT || 8888);
